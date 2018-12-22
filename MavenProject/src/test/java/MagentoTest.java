b5eacea51a4a68b01b7e863015185110a985610f
S masood <S masood@LAPTOP-9K4BP4SH>
2018-12-22 15:00:20 +0530

    first hhhhaaaa

---
+++ MavenProject/src/test/java/MagentoTest.java
@@ -0,0 +1,32 @@
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MagentoTest {
+	@Test
+	public  void credentials() {
+		 
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		 String url="https://www.magento.com";
+		 driver.get(url);
+		 Home h = new Home(driver);
+		 h.clickOnMyAcc();
+		 
+		 Login l1 = new Login(driver);
+		 l1.typeEmail("nitinmanjunath1991@gmail.com");
+		 l1.typePassword("Welcome123");
+		 l1.clickOnLogin();
+		  
+		 Logout l2=new Logout(driver);
+		 l2.clickOnLogout();
+		
+		 driver.quit();
+
+	}
+
+}
